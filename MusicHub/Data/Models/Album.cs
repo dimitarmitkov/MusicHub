d4a990d52300e4e0c3f70a2bfc757abbfa82103a
dimitarmitkov <55533699+dimitarmitkov@users.noreply.github.com>
2020-08-01 10:47:43 +0300

    Initial Upload

---
+++ MusicHub/Data/Models/Album.cs
@@ -0,0 +1,33 @@
+﻿namespace MusicHub.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+
+    public class Album
+    {
+        public Album()
+        {
+            this.Songs = new HashSet<Song>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3), MaxLength(40)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        public decimal Price => this.Songs.Sum(s => s.Price);
+
+        public int? ProducerId { get; set; }
+
+        public Producer Producer { get; set; }
+
+        public virtual ICollection<Song> Songs { get; set; }
+    }
+}
---
+++ MusicHub/Data/Models/Producer.cs
@@ -0,0 +1,28 @@
+﻿namespace MusicHub.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Producer
+    {
+        public Producer()
+        {
+            this.Albums = new HashSet<Album>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3), MaxLength(30)]
+        public string Name { get; set; }
+
+        [RegularExpression(@"^[A-Z]{1}[a-z]+[ ][A-Z]{1}[a-z]+$")]
+        public string Pseudonym { get; set; }
+
+        [RegularExpression(@"^[\+][\d]{3}([ ]{1}[\d]{3})(?1)(?1)$")]
+        public string PhoneNumber { get; set; }
+
+        public ICollection<Album> Albums { get; set; }
+    }
+}
