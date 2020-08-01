78437be97773e0fd1c819946514394dbb3818a42
dimitarmitkov <55533699+dimitarmitkov@users.noreply.github.com>
2020-08-02 00:16:56 +0300

    Serializer

--- MusicHub/StartUp.cs
+++ MusicHub/StartUp.cs
@@ -16,12 +16,12 @@
 
             Mapper.Initialize(config => config.AddProfile<MusicHubProfile>());
 
-            ResetDatabase(context, shouldDropDatabase: true);
+            //ResetDatabase(context, shouldDropDatabase: true);
 
             var projectDir = GetProjectDirectory();
 
-            ImportEntities(context, projectDir + @"Datasets/", projectDir + @"ImportResults/");
-            //ExportEntities(context, projectDir + @"ExportResults/");
+            //ImportEntities(context, projectDir + @"Datasets/", projectDir + @"ImportResults/");
+            ExportEntities(context, projectDir + @"ExportResults/");
 
             using (var transaction = context.Database.BeginTransaction())
             {
